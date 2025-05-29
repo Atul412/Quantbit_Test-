457647c6f373be1a57c01e9ab0e534cdc06a3de6
Atul412 <168251445+Atul412@users.noreply.github.com>
2025-05-29 12:58:04 +0530

    Update KthLargestElement.py

--- KthLargestElement.py
+++ KthLargestElement.py
@@ -3,6 +3,7 @@ def find_kth_largest_ele(arr,k):
     return arr[-k]
 print("Enter the elements array: ");
 arr = list(map(int,input().split()))
+print("Enter the Kth element: ");
 k = int(input())
 ans = find_kth_largest_ele(arr,k)
 print(ans)
